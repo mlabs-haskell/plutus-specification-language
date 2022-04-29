217d468cdba2148c8bd47bca28762a47d1941820
Márton Boros <martonboros@gmail.com>
2022-04-29 18:19:34 +0300

    fix ci

--- flake.nix
+++ flake.nix
@@ -7,21 +7,26 @@
   inputs.nixpkgs.follows = "idris/nixpkgs";
   inputs.flake-utils.follows = "idris/flake-utils";
 
-  outputs = { self, nixpkgs, idris, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
-    let pkgs = nixpkgs.legacyPackages.${system}; in
-    rec {
-      packages = idris.buildIdris.${system} {
-        projectName = "psl";
-        src = "${self}";
-        idrisLibraries = [];
-      };
-      defaultPackage = packages.build;
-      devShell = pkgs.mkShell {
-        buildInputs = [ idris.packages.${system}.idris2 pkgs.rlwrap ];
-        shellHook = ''
-          alias idris2="rlwrap -s 1000 idris2 --no-banner"
-        '';
-      };
-    }
-  );
+  outputs = { self, nixpkgs, idris, flake-utils }: flake-utils.lib.eachDefaultSystem
+    (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      rec {
+        packages = idris.buildIdris.${system}
+          {
+            projectName = "psl";
+            src = "${self}";
+            idrisLibraries = [ ];
+          } // {
+          default = packages.installLibrary;
+        };
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ idris.packages.${system}.idris2 pkgs.rlwrap ];
+          shellHook = ''
+            alias idris2="rlwrap -s 1000 idris2 --no-banner"
+          '';
+        };
+      }
+    ) // {
+    herculesCI.ciSystems = [ "x86_64-linux" ];
+  };
 }
