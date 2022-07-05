178ff223dc6343857ae81e7873fe215e37e9c6ee
hariamoor-mlabs <hari@mlabs.city>
2022-07-05 15:02:22 -0400

    Fix typechecking

--- examples/TicTacToe.hs
+++ examples/TicTacToe.hs
@@ -13,10 +13,10 @@ data Grid
       (Slot, Slot, Slot)
       (Slot, Slot, Slot)
 
-data GameDatum f = GameDatum
-  { state :: Ef f Grid
-  , redPkh :: Ef f EPubKeyHash
-  , bluePkh :: Ef f EPubKeyHash
+data GameDatum ef = GameDatum
+  { state :: ef /$ Grid
+  , redPkh :: ef /$ EPubKeyHash
+  , bluePkh :: ef /$ EPubKeyHash
   }
   deriving stock (Generic)
   deriving anyclass (EIsNewtype)
@@ -25,26 +25,27 @@ data Turn = (,) Int Int
   deriving stock (Generic)
   deriving anyclass (EIsNewtype)
 
-data GameCase f where
-  GameInit :: GameCase f
-  RedPlay :: Ef f GameDatum -> Ef f Turn -> GameCase f
-  BluePlay :: Ef f GameDatum -> Ef f Turn -> GameCase f
-  RedWon :: GameCase f
-  BlueWon :: GameCase f
+data GameCase ef where
+  GameInit ::
+    Term edsl EPubKeyHash ->
+    Term edsl EPubKeyHash ->
+    Term edsl EInteger ->
+    GameCase ef
+  RedPlay :: ef /$ GameDatum -> ef /$ Turn -> GameCase ef
+  BluePlay :: ef /$ GameDatum -> ef /$ Turn -> GameCase ef
+  RedWon :: GameCase ef
+  BlueWon :: GameCase ef
   deriving stock (Generic)
   deriving anyclass (EIsNewtype)
 
 gameCases ::
   EPSL edsl =>
-  Term edsl EPubKeyHash ->
-  Term edsl EPubKeyHash ->
-  Term edsl EInteger ->
   Term edsl GameCase ->
   Term edsl (EDiagram GameDatum)
-gameCases redPkh bluePkh adaCount =
+gameCases =
   ( `ematch`
       \case
-        GameInit -> MonoidDo.do
+        GameInit redPkh bluePkh adaCount -> MonoidDo.do
           let initDatum = let initRow = (Blank, Blank, Blank) in GameDatum{state = (initRow, initRow, initRow), ..}
           let ownUtxo = econ $ EOwnUTXO (mkAda adaCount) $ econ initDatum
           requireOwnInput ownUtxo
