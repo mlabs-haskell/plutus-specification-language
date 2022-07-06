cbe7b5cff29c9f7793cd469412f055500feea78c
Mario Blažević <blamario@protonmail.com>
2022-07-06 14:02:46 +0200

    Switched to HKD representation

--- families/Families.hs
+++ families/Families.hs
@@ -1,19 +1,33 @@
-{-# LANGUAGE DataKinds, ExplicitForAll, PolyKinds, StandaloneKindSignatures, TypeFamilies, TypeOperators #-}
+{-# LANGUAGE DataKinds, ExplicitForAll, PolyKinds, RankNTypes, StandaloneKindSignatures,
+             TypeFamilies, TypeFamilyDependencies, TypeOperators #-}
 
 module Families where
 
+import Data.Functor.Const (Const)
 import Data.Kind (Type)
 
-class ValidatorScript (s :: script) where
+class ValidatorScript s where
   type Currency s :: k
   type Datum s    :: Type
-  type Redeemer s :: Type
+  type Redeemer s = (r :: Type) | r -> s
+
+type DApp :: fam -> Type
+type family DApp t
+
+type Economy :: fam -> Type
+type family Economy t
 
 class Transaction (t :: familie) where
-  type Inputs t  :: [Type]
-  type Mints t   :: [k]
-  type Outputs t :: [Type]
-  type Mints t = '[]
+  type Inputs t  :: (forall s -> Redeemer s -> Type) -> (Economy t -> Type) -> Type
+  type Mints t   :: (c -> Type) -> Type
+  type Outputs t :: (DApp t -> Type) -> (Economy t -> Type) -> Type
+  type Mints t = Const ()
+
+type Wallet :: c -> k -> (c -> Type) -> Type
+data Wallet c s w
+
+type InputWallet :: c -> (forall s -> Redeemer s -> Type) -> (c -> Type) -> Type
+data InputWallet c s w
 
 {-
 type Input :: forall (script :: k) -> Redeemer script -> Type
