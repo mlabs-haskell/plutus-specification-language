f9f2f21c251c4a7cbb2d2c5a0130c9eaef87769d
Mario Blažević <blamario@protonmail.com>
2022-08-02 14:37:49 +0200

    Introduced NodeId

--- families/Families/Diagram.hs
+++ families/Families/Diagram.hs
@@ -4,6 +4,7 @@ module Families.Diagram where
 
 import Families (Transaction)
 import HKD
+import Data.Bifunctor (first)
 import Data.Kind (Type)
 import Data.List (nub)
 import Data.Map (Map)
@@ -51,18 +52,18 @@ nodeType = (`mod` 7)
 isTransaction :: Int -> Bool
 isTransaction = (== 0) . nodeType
 
-transactionGraphToDot :: Gr Text Text -> DotGraph Int
+transactionGraphToDot :: Gr NodeId Text -> DotGraph Int
 transactionGraphToDot g = graphToDot params g'
   where g' :: Gr Text Text
-        g' = foldr dropNode g (filter ((> 4) . nodeType) $ map fst $ labNodes g) -- drop script and wallet nodes
-        dropNode :: Int -> Gr Text Text -> Gr Text Text
+        g' = first nodeLabel $ foldr dropNode g (filter ((> 4) . nodeType) $ map fst $ labNodes g) -- drop script and wallet nodes
+        dropNode :: Int -> Gr a b -> Gr a b
         dropNode n gr = snd (match n gr)
         params :: GraphvizParams Int Text Text Int Text
         params = defaultParams{
           clusterID = Num . Int,
           clusterBy = clustering,
           fmtCluster = \ n -> case match n g of
-            (Just (_, _, scriptName, _), _) -> [GraphAttrs [toLabel scriptName]],
+            (Just (_, _, node, _), _) -> [GraphAttrs [toLabel $ nodeLabel node]],
           fmtNode = \ (n, l) -> toLabel l : if isTransaction n then [Shape DoubleOctagon] else [],
           -- give wallet inputs weight 0 to move wallet clusters below transaction
           fmtEdge = \ (src, dest, l) ->
@@ -92,50 +93,70 @@ transactionGraphToDot g = graphToDot params g'
               -- transactions
               _ -> N (n, name)
 
-transactionTypeGraph :: Int -> TransactionTypeDiagram -> Gr Text Text
+data OverlayMode = Parallel | Serial
+
+data NodeId = ScriptNamed Text
+            | WalletNamed Text
+            | TransactionNamed Text
+            | ScriptUTxO {script :: Text, datum :: Text, currencies :: [Currency]}
+            | WalletUTxO Text [Currency]
+            deriving (Eq, Show)
+
+nodeLabel :: NodeId -> Text
+nodeLabel (ScriptNamed name) = name
+nodeLabel (WalletNamed name) = name
+nodeLabel (TransactionNamed name) = name
+nodeLabel ScriptUTxO {currencies} = Text.intercalate ", " (currencyName <$> currencies)
+nodeLabel (WalletUTxO _ currencies) = Text.intercalate ", " (currencyName <$> currencies)
+
+transactionTypeFamilyGraph :: OverlayMode -> [TransactionTypeDiagram] -> Gr Text Text
+transactionTypeFamilyGraph = undefined
+
+transactionTypeGraph :: Int -> TransactionTypeDiagram -> Gr NodeId Text
 transactionTypeGraph
   startIndex
   TransactionTypeDiagram{transactionName, scriptInputs, scriptOutputs, walletInputs, walletOutputs}
   = mkGraph nodes edges where
   nodes =
-    (transactionNode, transactionName)
-    : (map dropMiddles (isNodes <> osNodes) <> map dropMiddle (iwNodes <> owNodes) <> scriptNodes <> walletNodes)
-  edges = [(n, transactionNode, redeemer (scriptInputs Map.! name) <> "@" <> name) | (n, name, _, _) <- isNodes]
-       <> [(n, transactionNode, name) | (n, name, _) <- iwNodes]
-       <> [(transactionNode, n, name) | (n, name, _, _) <- osNodes]
-       <> [(transactionNode, n, name) | (n, name, _) <- owNodes]
+    (transactionNode, TransactionNamed transactionName)
+    : (isNodes <> osNodes <> iwNodes <> owNodes <> scriptNodes <> walletNodes)
+  edges = [ (n, transactionNode, redeemer <> "@" <> name)
+          | (name, InputFromScript{fromScript, redeemer, datum, currencies}) <- Map.toList scriptInputs,
+            let [(n, _)] = filter ((ScriptUTxO fromScript datum currencies ==) . snd) isNodes]
+       <> [ (n, transactionNode, name)
+          | (name, Wallet w currencies) <- Map.toList walletInputs,
+            let [(n, _)] = filter ((WalletUTxO w currencies ==) . snd) iwNodes]
+       <> [ (transactionNode, n, name)
+          | (name, OutputToScript{toScript, datum, currencies}) <- Map.toList scriptOutputs,
+            let [(n, _)] = filter ((ScriptUTxO toScript datum currencies ==) . snd) osNodes]
+       <> [ (transactionNode, n, name)
+          | (name, Wallet w currencies) <- Map.toList walletOutputs,
+            let [(n, _)] = filter ((WalletUTxO w currencies ==) . snd) owNodes]
        <> [ (scriptNode, n, "")
-          | (n, name, _, _) <- isNodes,
-            let scriptName = fromScript $ scriptInputs Map.! name;
-                [(scriptNode, _)] = filter ((scriptName ==) . snd) scriptNodes]
+          | (n, ScriptUTxO name _ _) <- isNodes,
+            let [(scriptNode, _)] = filter ((ScriptNamed name ==) . snd) scriptNodes]
        <> [ (scriptNode, n, "")
-          | (n, name, _, _) <- osNodes,
-            let scriptName = toScript $ scriptOutputs Map.! name;
-                [(scriptNode, _)] = filter ((scriptName ==) . snd) scriptNodes]
+          | (n, ScriptUTxO name _ _) <- osNodes,
+            let [(scriptNode, _)] = filter ((ScriptNamed name ==) . snd) scriptNodes]
        <> [ (walletNode, n, "")
-          | (n, name, _) <- iwNodes <> owNodes,
-            let wName = walletName $ walletInputs Map.! name;
-                [(walletNode, _)] = filter ((wName ==) . snd) walletNodes]
+          | (n, WalletUTxO name _) <- iwNodes <> owNodes,
+            let [(walletNode, _)] = filter ((WalletNamed name ==) . snd) walletNodes]
   transactionNode = 7*startIndex
   isNodes =
-    [ (7*n+1, name, datum, present currencies)
-    | (n, (name, InputFromScript {currencies, datum})) <- zip [startIndex..] $ Map.toList scriptInputs ]
+    [ (7*n+1, ScriptUTxO fromScript datum currencies)
+    | (n, (name, InputFromScript {fromScript, datum, currencies})) <- zip [startIndex..] $ Map.toList scriptInputs ]
   osNodes =
-    [ (7*n+2, name, datum, present currencies)
-    | (n, (name, OutputToScript {currencies, datum})) <- zip [startIndex..] $ Map.toList scriptOutputs]
+    [ (7*n+2, ScriptUTxO toScript datum currencies)
+    | (n, (name, OutputToScript {toScript, datum, currencies})) <- zip [startIndex..] $ Map.toList scriptOutputs]
   iwNodes =
-    [ (7*n+3, name, present currencies)
-    | (n, (name, Wallet {currencies})) <- zip [startIndex..] $ Map.toList walletInputs]
+    [ (7*n+3, WalletUTxO walletName currencies)
+    | (n, (name, Wallet {walletName, currencies})) <- zip [startIndex..] $ Map.toList walletInputs]
   owNodes =
-    [ (7*n+4, name, present currencies)
-    | (n, (name, Wallet {currencies})) <- zip [startIndex..] $ Map.toList walletOutputs]
+    [ (7*n+4, WalletUTxO walletName currencies)
+    | (n, (name, Wallet {walletName, currencies})) <- zip [startIndex..] $ Map.toList walletOutputs]
   scriptNodes =
-    [ (7*n+5, name)
+    [ (7*n+5, ScriptNamed name)
     | (n, name) <- zip [0..] $ nub $ map snd $ Map.toList ((fromScript <$> scriptInputs) <> (toScript <$> scriptOutputs)) ]
-  walletNodes = 
-    [ (7*n+6, name)
+  walletNodes =
+    [ (7*n+6, WalletNamed name)
     | (n, name) <- zip [0..] $ nub $ map snd $ Map.toList (walletName <$> walletInputs <> walletOutputs) ]
-  dropMiddle (a, _, b) = (a, b)
-  dropMiddles (a, _, _, b) = (a, b)
-  present :: [Currency] -> Text
-  present = Text.intercalate ", " . map currencyName
